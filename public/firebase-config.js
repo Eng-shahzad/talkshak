92ae169f31c736e9a7efa6fd46816772f9717e54
Eng-shahzad <shahzadrashd404@gmail.com>
2025-10-29 00:54:21 -0600

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,3 +1,8 @@
+// Updated for Firebase v9+ (modular SDK)
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCmHtVXzcN4BIs4Eu33uSJ1drXlnwDypqc",
   authDomain: "talkshak-app.firebaseapp.com",
@@ -7,3 +12,7 @@ const firebaseConfig = {
   appId: "1:507355222941:web:030ec6a86f2d0b5cc19574",
   measurementId: "G-EM4CRY9B5X"
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
